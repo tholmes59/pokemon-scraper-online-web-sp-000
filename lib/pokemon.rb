02b2049f8ca33ebb2a214760be804a452f0ffbad
Tom Holmes <tholmes59@gmail.com>
2019-02-22 17:42:19 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,23 @@
 class Pokemon
 end
+
+
+# class Cat
+ 
+#   @@all = []
+ 
+#   def initialize(name, breed, age)
+#     @name = name
+#     @breed = breed
+#     @age = age
+#     @@all << self
+#   end
+ 
+#   def self.all
+#     @@all
+#   end
+ 
+#   def self.save(name, breed, age, database_connection)
+#     database_connection.execute("INSERT INTO cats (name, breed, age) VALUES (?, ?, ?)",name, breed, age)
+#   end
+# end
